29fa89b84109aac0831224347b7f0c3ba5e2bc7c
Kirill Kniga <kiruha_95@icloud.com>
2023-04-04 14:23:34 +0300

    Initial Commit

---
+++ MyTaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTaskList
+//
+//  Created by Kirill Kniga on 04.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
